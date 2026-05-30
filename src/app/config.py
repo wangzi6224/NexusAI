1b3f9da4f79a9b42eb1a2353e6a38b9eddc1600f
zilong <wangzi6224@gmail.com>
2026-05-30 17:44:25 +0800

    feat: 将 .env.api_keys 添加到 .gitignore 文件中，以防止其被跟踪

--- src/app/config.py
+++ src/app/config.py
@@ -110,7 +110,7 @@ class Settings(BaseSettings):
     )
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=[".env", ".env.api_keys"],
         env_file_encoding="utf-8",
         extra="ignore",
     )
